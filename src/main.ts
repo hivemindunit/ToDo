0b1b5fb5e8612301ca1637aeaf962018eac7ed3f
Alexander Tsimbalistov <atsimbalistov@gmail.com>
2020-05-11 20:22:17 +0300

    Testing

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,13 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import Amplify, { Auth } from 'aws-amplify';
+import Amplify, { DataStore } from 'aws-amplify';
 import API from '@aws-amplify/api';
 import awsmobile from './aws-exports.js';
 
 Amplify.configure(awsmobile);
 API.configure(awsmobile);
+DataStore.configure();
 
 if (environment.production) {
   enableProdMode();
