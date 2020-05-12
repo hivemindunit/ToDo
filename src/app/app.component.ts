b23ceea61023f45cc5b4fb4763654239ccb3eedb
Alexander Tsimbalistov <atsimbalistov@gmail.com>
2020-05-12 21:44:49 +0300

    Back button workaround

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import {Component, HostListener} from '@angular/core';
+import {Component, QueryList, ViewChildren} from '@angular/core';
 
-import { Platform, NavController } from '@ionic/angular';
+import {Platform, IonRouterOutlet, ModalController, MenuController, ActionSheetController, PopoverController} from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-import {BackButtonService} from './back-button.service';
+import {Router} from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -11,18 +12,28 @@ import {BackButtonService} from './back-button.service';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
-  @HostListener('document:backbutton')
-  backButtonService: BackButtonService;
+  // set up hardware back button event.
+  lastTimeBackPress = 0;
+  timePeriodToExit = 2000;
+
+  @ViewChildren(IonRouterOutlet) routerOutlets: QueryList<IonRouterOutlet>;
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
-    private navCtrl: NavController
-  ) {
+    public modalCtrl: ModalController,
+    private menu: MenuController,
+    private actionSheetCtrl: ActionSheetController,
+    private popoverCtrl: PopoverController,
+    private router: Router,
+    private toast: ToastController) {
     this.platform.backButton.subscribeWithPriority(10, () => {
       console.log('Handler was called!');
     });
     this.initializeApp();
+    // Initialize BackButton Eevent.
+    this.backButtonEvent();
   }
 
   initializeApp() {
@@ -32,11 +43,74 @@ export class AppComponent {
     });
   }
 
-  onBackButton() {
-    if (this.backButtonService.quitOnBackButton) {
-      this.backButtonService.closeApp();
-    } else {
-      this.navCtrl.back();
-    }
+  // active hardware back button
+  backButtonEvent() {
+    this.platform.backButton.subscribe(async () => {
+      // close action sheet
+      try {
+        const element = await this.actionSheetCtrl.getTop();
+        if (element) {
+          element.dismiss();
+          return;
+        }
+      } catch (error) {
+      }
+
+      // close popover
+      try {
+        const element = await this.popoverCtrl.getTop();
+        if (element) {
+          element.dismiss();
+          return;
+        }
+      } catch (error) {
+      }
+
+      // close modal
+      try {
+        const element = await this.modalCtrl.getTop();
+        if (element) {
+          element.dismiss();
+          return;
+        }
+      } catch (error) {
+        console.log(error);
+
+      }
+
+      // close side menua
+      try {
+        const element = await this.menu.getOpen();
+        if (element) {
+          this.menu.close();
+          return;
+
+        }
+
+      } catch (error) {
+
+      }
+
+      this.routerOutlets.forEach((outlet: IonRouterOutlet) => {
+        if (outlet && outlet.canGoBack()) {
+          outlet.pop();
+
+        } else if (this.router.url === '/home') {
+          if (new Date().getTime() - this.lastTimeBackPress < this.timePeriodToExit) {
+            // this.platform.exitApp(); // Exit from app
+            navigator['app'].exitApp(); // work in ionic 4
+
+          } else {
+            // const toast = this.toast.create({
+            //   message: 'Press back again to exit App',
+            //   duration: 2000,
+            //   position: 'middle'});
+            // toast.present();
+            console.log('Press back again to exit App');
+            this.lastTimeBackPress = new Date().getTime();
+          }
+        }
+      });
+    });
   }
 }
