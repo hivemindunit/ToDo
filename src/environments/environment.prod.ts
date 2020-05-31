66d3b3952b7899c1a1c7e6e329e0f36c5affa336
Alexander Tsimbalistov <atsimbalistov@gmail.com>
2020-05-31 12:37:16 +0300

    Production config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,13 +2,13 @@ export const environment = {
   production: true,
   appUrl: 'https://get-it-done.ninja',
   firebaseConfig: {
-    apiKey: 'AIzaSyCILy4z9vwGUrAf8K9XswDDHxFidQ7NiMw',
-    authDomain: 'todo-together-1588410185849.firebaseapp.com',
-    databaseURL: 'https://todo-together-1588410185849.firebaseio.com',
-    projectId: 'todo-together-1588410185849',
-    storageBucket: 'todo-together-1588410185849.appspot.com',
-    messagingSenderId: '469650996905',
-    appId: '1:469650996905:web:f1610b6d0a636dda47f3b8',
-    measurementId: 'G-2PQQ5HBHMF'
+    apiKey: 'AIzaSyAlr1D31kKAvZBTKqrMuPPEUz_gXg8tw0s',
+    authDomain: 'get-it-done-5a34c.firebaseapp.com',
+    databaseURL: 'https://get-it-done-5a34c.firebaseio.com',
+    projectId: 'get-it-done-5a34c',
+    storageBucket: 'get-it-done-5a34c.appspot.com',
+    messagingSenderId: '348414221184',
+    appId: '1:348414221184:web:3b2ef94dae8d3a206fbf65',
+    measurementId: 'G-C2FJ0JDRB9'
   }
 };
